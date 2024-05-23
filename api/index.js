f214d75ef30f57d11c8aaab203166a72295414bf
offline <vickymuthunga@gmail.com>
2024-05-23 20:12:20 +0300

    completed the basic set up

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
+
 import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
@@ -17,7 +15,7 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,15 +27,13 @@ app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// app.get("*", (req, res, next) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <> <h1>Dairy farming </h1>
+            <p>For Tommorow Betterment</p>
+        </>
+    );
+}
+
+export default Home;
